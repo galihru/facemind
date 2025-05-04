a451f603bdd74eef35c1b37d1ecbed6f138c45d3
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-05-05 01:10:14 +0700

    Update dfs.js

--- dfs.js
+++ dfs.js
@@ -1,58 +1,70 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const installButton = document.getElementById('installButton');
-  const modal = document.getElementById('swipeableModal');
-  const cancelInstall = document.getElementById('cancelInstall');
-  const modalContent = modal.querySelector('.modal-content');
-  let startY;
+    const installButton = document.getElementById('installButton');
+    const modal = document.getElementById('swipeableModal');
+    const cancelInstall = document.getElementById('cancelInstall');
 
-  installButton.onclick = function () {
-      modal.classList.add('show');
-      document.body.classList.add('modal-open');
-      modalContent.classList.add('translate-reset');
-  };
+    // Cek apakah modal ada
+    if (!modal) {
+        console.error('Element with id "swipeableModal" not found.');
+        return;
+    }
 
-  cancelInstall.onclick = function () {
-      closeModal();
-  };
+    const modalContent = modal.querySelector('.modal-content');
+    if (!modalContent) {
+        console.error('Element ".modal-content" not found inside #swipeableModal.');
+        return;
+    }
 
-  modal.addEventListener('touchstart', function (e) {
-      startY = e.touches[0].clientY;
-  });
+    let startY;
 
-  modal.addEventListener('touchmove', function (e) {
-      const currentY = e.touches[0].clientY;
-      const diffY = currentY - startY;
+    installButton.onclick = function () {
+        modal.classList.add('show');
+        document.body.classList.add('modal-open');
+        modalContent.classList.add('translate-reset');
+    };
 
-      if (diffY > 0) {
-          modalContent.classList.add('translate-drag');
-          modalContent.style.setProperty('--dragY', `${diffY}px`);
-      }
-  });
+    cancelInstall.onclick = function () {
+        closeModal();
+    };
 
-  modal.addEventListener('touchend', function (e) {
-      const endY = e.changedTouches[0].clientY;
-      const diffY = endY - startY;
+    modal.addEventListener('touchstart', function (e) {
+        startY = e.touches[0].clientY;
+    });
 
-      modalContent.classList.remove('translate-drag');
+    modal.addEventListener('touchmove', function (e) {
+        const currentY = e.touches[0].clientY;
+        const diffY = currentY - startY;
 
-      if (diffY > 100) {
-          closeModal();
-      } else {
-          modalContent.classList.add('translate-reset');
-          modalContent.style.setProperty('--dragY', `0px`);
-      }
-  });
+        if (diffY > 0) {
+            modalContent.classList.add('translate-drag');
+            modalContent.style.setProperty('--dragY', `${diffY}px`);
+        }
+    });
 
-  function closeModal() {
-      modalContent.classList.remove('translate-reset', 'translate-drag');
-      modalContent.classList.add('translate-hide');
+    modal.addEventListener('touchend', function (e) {
+        const endY = e.changedTouches[0].clientY;
+        const diffY = endY - startY;
 
-      setTimeout(() => {
-          modal.classList.remove('show');
-          document.body.classList.remove('modal-open');
-          modalContent.classList.remove('translate-hide');
-          modalContent.classList.add('translate-reset');
-          modalContent.style.removeProperty('--dragY');
-      }, 300);
-  }
+        modalContent.classList.remove('translate-drag');
+
+        if (diffY > 100) {
+            closeModal();
+        } else {
+            modalContent.classList.add('translate-reset');
+            modalContent.style.setProperty('--dragY', `0px`);
+        }
+    });
+
+    function closeModal() {
+        modalContent.classList.remove('translate-reset', 'translate-drag');
+        modalContent.classList.add('translate-hide');
+
+        setTimeout(() => {
+            modal.classList.remove('show');
+            document.body.classList.remove('modal-open');
+            modalContent.classList.remove('translate-hide');
+            modalContent.classList.add('translate-reset');
+            modalContent.style.removeProperty('--dragY');
+        }, 300);
+    }
 });
