842794cc4e6c8a95d54f87a09efde7b329859396
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-29 07:09:25 +0700

    Update __init__.py

--- mental_health_app/__init__.py
+++ mental_health_app/__init__.py
@@ -1,4 +1,3 @@
-# mental_health_app/__init__.py
 from .main import save_to_database, show_graph, SplashScreen, LoginWindow, MentalHealthPredictor
 
 __all__ = ["save_to_database", "show_graph", "SplashScreen", "LoginWindow", "MentalHealthPredictor"]
